461a94883a1d5502ebb41e8c9e5cffd505b85b66
Technostalgic <isaiahnsmith@hotmail.com>
2018-01-26 03:22:32 -0700

    got working rendering infastructure

--- js/game.js
+++ js/game.js
@@ -5,11 +5,22 @@
 ///	twitter @technostalgicGM
 ///
 
+///
 /// ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ }Global variables{ ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ 
 var debug = true;
-var config = {};
-var gfx = {};
+	localStorageEnabled = true;
+	timeElapsed = 0;
+var config = {},
+	controls = {};
+var gfx = {},
 	sfx = {};
+
+// canvas and contexts
+var renderTarget,
+	scalingTarget;
+var renderContext,
+	scalingContext;
+///
 /// ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ }Global functions{ ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 function log(obj = "@ console logged"){
 	if(!debug) return;
@@ -40,8 +51,18 @@ function loadSound(assetname, filename){
 	log(out);
 	return r;
 }
+function clearScreen(color = "#aaa"){
+	renderContext.fillStyle = color;
+	scalingContext.fillStyle = color;
+	
+	renderContext.fillRect(0, 0, renderTarget.width, renderTarget.height);
+	scalingContext.fillRect(0, 0, scalingTarget.width, scalingTarget.height);
+}
+///
 /// ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ }High-Level functions{ ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 function init(){
+	loadConfig();
+	loadControls();
 	loadAssets();
 	log("intitialized game!");
 }
@@ -70,6 +91,67 @@ function loadSFX(){
 	
 	log(Object.keys(sfx).length.toString() + " files indexed");
 }
+function loadConfig(){
+	log("loading game configuration... ");
+	setDefaultConfig();
+}
+function loadControls(){
+	log("loading controls... ");
+	setDefaultControls();
+}
+
+function setDefaultConfig(){
+	config = {
+		animSpeed: 100,
+		music: true,
+		sound: true,
+		canvasSize: 800,
+		imageSmoothing: false,
+	};
+}
+function setDefaultControls(){
+	controls = {
+		ballLeft: 0,
+		ballRight: 0,
+		ballDown: 0,
+		ballUp: 0,
+		bumpLeft: 0,
+		bumpRight: 0,
+		bumpDown: 0,
+		quickDrop: 0,
+		rotateCW: 0,
+		rotateCCW: 0,
+		select: 0,
+		pause: 0
+	};
+}
+
+function applyConfig(){
+	var smoothing = config.imageSmoothing;
+	
+	renderContext.mozImageSmoothingEnabled    = smoothing;
+	renderContext.oImageSmoothingEnabled      = smoothing;
+	renderContext.webkitImageSmoothingEnabled = smoothing;
+	renderContext.msImageSmoothingEnabled     = smoothing;
+	renderContext.imageSmoothingEnabled       = smoothing;
+	
+	scalingContext.mozImageSmoothingEnabled    = smoothing;
+	scalingContext.oImageSmoothingEnabled      = smoothing;
+	scalingContext.webkitImageSmoothingEnabled = smoothing;
+	scalingContext.msImageSmoothingEnabled     = smoothing;
+	scalingContext.imageSmoothingEnabled       = smoothing;
+}
+function getCanvas(){
+	log("retrieving canvas data... ");
+	
+	scalingTarget = document.getElementById("canvas");
+	scalingContext = scalingTarget.getContext("2d");
+	
+	renderTarget = document.createElement("canvas");
+	renderTarget.width = 600;
+	renderTarget.height = 800;
+	renderContext = renderTarget.getContext("2d");
+}
 
 function assetLoadingFinishCheck(){
 	for(var i in gfx){
@@ -89,7 +171,29 @@ function assetLoadingFinishCheck(){
 }
 function finishLoading(){
 	log("--> finished loading game! " + (performance.now()).toString() + "ms elapsed");
+	getCanvas();
+	applyConfig();
+	startGameLoop();
+}
+
+function startGameLoop(){
+	log("initiating game loop...");
+	window.requestAnimationFrame(step);
+}
+function step(){
+	var dt = performance.now() - timeElapsed;
+	
+	update(dt);
+	draw();
+	
+	window.requestAnimationFrame(step);
+	timeElapsed = performance.now();
+}
+function update(dt){}
+function draw(){
+	clearScreen();
 }
 /// ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ { ------------------ } ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+///
 
 window.addEventListener("load", init);
