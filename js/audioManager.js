97f1515b6243f20d31c990eb10993ae93c78abc8
Technostalgic <Isaiahnsmith@hotmail.com>
2018-05-17 10:48:28 -0600

    fixed bug where playMusicWhenPossible() from past callbacks would take precedince over playMusic() in audio manager class

--- js/audioManager.js
+++ js/audioManager.js
@@ -10,6 +10,7 @@ class audioMgr{
 	static init(){
 		// initialize static fields
 		audioMgr.currentMusic = null;
+		audioMgr.musicPlayCallback = null;
 	}
 	
 	static playSound(sound, forceRestart = true){
@@ -36,12 +37,15 @@ class audioMgr{
 		// ensures that the music will begin playback when the browser allows it to
 		if(track.paused)
 			audioMgr.playMusicWhenPossible(track);
+		else if(audioMgr.musicPlayCallback != null)
+			clearTimeout(audioMgr.musicPlayCallback);
 	}
 	static playMusicWhenPossible(track){
 		// some browsers prevent playback of audio under certain circumstances. This ensures that the
 		// music will be played as soon as the browser allows it to if the initial playback request is
 		// denied
 		if(track.paused)
+			audioMgr.musicPlayCallback = 
 			setTimeout(function(){
 				audioMgr.playMusic(track);
 			});
