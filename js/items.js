39417d09098b5aaa82a478b251eb8cdae4c4b86e
Technostalgic <Isaiahnsmith@hotmail.com>
2018-05-29 20:25:18 -0600

    replaced generic item with coin

--- js/items.js
+++ js/items.js
@@ -13,23 +13,7 @@ class item{
 	}
 	
 	static getItem_random(){
-		var itemNames = [
-			"getItem_extraItems",
-			"getItem_extraPoints",
-			"getItem_rollingBomb",
-			"getItem_detonateBricks"
-		];
-
-		var itm = Math.floor(Math.random() * itemNames.length);
-		return item[itemNames[itm]]();
-	}
-	static getItem_extraItems(){
-		// returns the 'extra items' item
-		var r = new item();
-		r.icon = 0;
-		r.m_activate = item.ACT_extraItems;
-		r.iconAnim = item.anim_pulsate();
-		return r;
+		return new item();
 	}
 	static getItem_extraPoints(){
 		// returns the 'extra points' item
@@ -38,35 +22,6 @@ class item{
 		r.m_activate = item.ACT_extraPoints;
 		r.iconAnim = item.anim_pulsate();
 		return r;
-	}
-	static getItem_rollingBomb(){
-		var r = new item();
-		r.icon = 2;
-		r.m_activate = item.ACT_rollingBomb;
-		r.iconAnim = item.anim_pulsate();
-		return r;
-	}
-	static getItem_detonateBricks(){
-		var r = new item();
-		r.icon = 3;
-		r.m_activate = item.ACT_detonateBricks;
-		r.iconAnim = item.anim_pulsate();
-		return r;
-	}
-	
-	static anim_pulsate(){
-		// returns a pulsating animation
-		var r = new textAnim_scale();
-		r.animLength = 1000;
-		r.minScale = 0.85;
-		r.maxScale = 1.15;
-		r.looping = true;
-		r.animType = textAnimType.trigonometricCycle;
-		
-		return r;
-	}
-	static anim_teeter(){
-		return new textAnim_rotationOffset(1000);
 	}
 
 	get gridPos(){
@@ -95,63 +50,10 @@ class item{
 	activate(ballOb){
 		// activates the item, ballOb should be the ball object that touched it to activate it
 		this.destroy();
-		this.m_activate(this, ballOb);
-	}
-
-	m_activate(self, ballOb){}
-	static ACT_extraItems(self, ballOb){
-		// the activation method for the extra items item
-		// get all the tube tiles but remove the ones that are already tagged
-		var ttiles = tile.getAllTilesOfType(entities.tube);
-		for(let i = ttiles.length - 1; i >= 0; i--){
-			if(ttiles[i].tagged){
-				ttiles.splice(i, 1);
-			}
-		}
-		
-		// spawn 2 items if on level 1-10, otherwise spawn 3
-		var count = gameState.current.currentLevel.difficulty > 10 ? 3 : 2;
-		
-		// if the amount of items that are being spawned is greater than or equal to the amount
-		// of tiles that they can be spawned on, just spawn random items on all the tiles available
-		if(ttiles.length <= count){
-			ttiles.forEach(function(tileOb){
-				tileOb.setItem(item.getItem_random());
-			});
-			return;
-		}
-		
-		// push x amount of items to a random tube tile in the tile grid
-		var iposes = [];
-		for(let i = count; i > 0; i--){
-			let ipos = Math.floor(ttiles.length * Math.random());
-			while(iposes.includes(ipos)) ipos = Math.floor(ttiles.length * Math.random());
-			ttiles[ipos].setItem(item.getItem_random());
-			iposes.push(ipos);
-		}
-	}
-	static ACT_extraPoints(self, ballOb){
-		// the activation method for the extra points item
 		scoring.addScore(
 			300 + 50 * gameState.current.currentLevel.difficulty, 
-			tile.toScreenPos(self.parentTile.gridPos),
+			tile.toScreenPos(this.parentTile.gridPos),
 			scoreTypes.bonus);
-	}
-	static ACT_rollingBomb(self, ballOb){
-		ballOb.addDestroyEventListener(function(self){
-			tile.explodeAt(new vec2(self.gridPos.x, self.gridPos.y));
-		});
-	}
-	static ACT_detonateBricks(self, ballOb){
-		var bricksDetonated = 0;
-		var iterator = function(tileOb, x, y){
-			if(tileOb.isEntity(blocks.brick, entities.block)){
-				tile.explodeAt(new vec2(x, y), false);
-				bricksDetonated++;
-				scoring.addScore(bricksDetonated *= 200, tile.toScreenPos(new vec2(x, y)), scoreTypes.bonus);
-			}
-		};
-		tile.iterateGrid(iterator);
 	}
 	
 	draw(pos){
